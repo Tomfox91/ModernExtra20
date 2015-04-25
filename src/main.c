a27dceb12d9eea2d059f2c3e316afcf93b7047ae
Tommaso Frassetto <tomfox91@libero.it>
2015-04-25 23:48:36 +0200

    Sunset time, Italian date.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include "pebble.h"
-#include "common.h"
+#include "tx_generated.h"
 
 static Window* window;
 static GBitmap *background_image_container;
@@ -7,9 +7,10 @@ static GBitmap *background_image_container;
 static Layer *minute_display_layer;
 static Layer *hour_display_layer;
 static Layer *center_display_layer;
-static Layer *second_display_layer;
 static TextLayer *date_layer;
-static char date_text[] = "Wed 13 ";
+static char date_text[] = "Xxx 88 Xxx";
+static TextLayer *sunset_layer;
+static char sunset_text[] = "88:88";
 static bool bt_ok = false;
 static uint8_t battery_level;
 static bool battery_plugged;
@@ -21,7 +22,7 @@ static GBitmap *icon_bt;
 static Layer *battery_layer;
 static Layer *bt_layer;
 
-bool g_conserve = false;
+#define GRECT_FULL_WINDOW GRect(0,0,144,168)
 
 #ifdef INVERSE
 static InverterLayer *full_inverse_layer;
@@ -46,7 +47,6 @@ static int my_cookie = COOKIE_MY_TIMER;
 #define ANIM_START 1
 #define ANIM_HOURS 2
 #define ANIM_MINUTES 3
-#define ANIM_SECONDS 4
 #define ANIM_DONE 5
 int init_anim = ANIM_DONE;
 int32_t second_angle_anim = 0;
@@ -70,49 +70,9 @@ void handle_timer(void* vdata) {
 			layer_mark_dirty(minute_display_layer);
 			timer_handle = app_timer_register(50 /* milliseconds */,
 					&handle_timer, &my_cookie);
-		} else if (init_anim == ANIM_SECONDS) {
-			layer_mark_dirty(second_display_layer);
-			timer_handle = app_timer_register(50 /* milliseconds */,
-					&handle_timer, &my_cookie);
 		}
 	}
 
-}
-
-void second_display_layer_update_callback(Layer *me, GContext* ctx) {
-	(void) me;
-
-	time_t now = time(NULL);
-	struct tm *t = localtime(&now);
-
-	int32_t second_angle = t->tm_sec * (0xffff / 60);
-	int second_hand_length = 70;
-	GPoint center = grect_center_point(&GRECT_FULL_WINDOW);
-	GPoint second = GPoint(center.x, center.y - second_hand_length);
-
-	if (init_anim < ANIM_SECONDS) {
-		second = GPoint(center.x, center.y - 70);
-	} else if (init_anim == ANIM_SECONDS) {
-		second_angle_anim += 0xffff / 60;
-		if (second_angle_anim >= second_angle) {
-			init_anim = ANIM_DONE;
-			second =
-					GPoint(center.x + second_hand_length * sin_lookup(second_angle)/0xffff,
-							center.y + (-second_hand_length) * cos_lookup(second_angle)/0xffff);
-		} else {
-			second =
-					GPoint(center.x + second_hand_length * sin_lookup(second_angle_anim)/0xffff,
-							center.y + (-second_hand_length) * cos_lookup(second_angle_anim)/0xffff);
-		}
-	} else {
-		second =
-				GPoint(center.x + second_hand_length * sin_lookup(second_angle)/0xffff,
-						center.y + (-second_hand_length) * cos_lookup(second_angle)/0xffff);
-	}
-
-	graphics_context_set_stroke_color(ctx, GColorWhite);
-
-	graphics_draw_line(ctx, center, second);
 }
 
 void center_display_layer_update_callback(Layer *me, GContext* ctx) {
@@ -137,11 +97,7 @@ void minute_display_layer_update_callback(Layer *me, GContext* ctx) {
 		angle = 0;
 	} else if (init_anim == ANIM_MINUTES) {
 		minute_angle_anim += 6;
-		if (minute_angle_anim >= angle) {
-			init_anim = ANIM_SECONDS;
-		} else {
-			angle = minute_angle_anim;
-		}
+		init_anim = ANIM_DONE;
 	}
 
 	gpath_rotate_to(minute_hand_path, (TRIG_MAX_ANGLE / 360) * angle);
@@ -185,13 +141,17 @@ void hour_display_layer_update_callback(Layer *me, GContext* ctx) {
 }
 
 void draw_date() {
-
 	time_t now = time(NULL);
-	struct tm *t = localtime(&now);
+	struct tm* t = localtime(&now);
+	char* weekdays[] = {"dom", "lun", "mar", "mer", "gio", "ven", "sab"};
+	char* months[] = {"gen", "feb", "mar", "apr", "mag", "giu", "lug", "ago", "set", "ott", "nov", "dic"};
 
-	strftime(date_text, sizeof(date_text), "%a %d", t);
-
+	snprintf(date_text, sizeof(date_text), "%s %d %s", weekdays[t->tm_wday], t->tm_mday, months[t->tm_mon]);
 	text_layer_set_text(date_layer, date_text);
+}
+
+void draw_sunset() {
+	text_layer_set_text(sunset_layer, sunset_text);
 }
 
 /*
@@ -217,12 +177,6 @@ void battery_state_handler(BatteryChargeState charge) {
 	battery_level = charge.charge_percent;
 	battery_plugged = charge.is_plugged;
 	layer_mark_dirty(battery_layer);
-	/*
-	if (!battery_plugged && battery_level < 20)
-		conserve_power(true);
-	else
-		conserve_power(false);
-	*/
 }
 
 /*
@@ -261,15 +215,21 @@ void init() {
 	layer_set_update_proc(background_layer, &draw_background_callback);
 	layer_add_child(window_layer, background_layer);
 
-	// Date setup
-	date_layer = text_layer_create(GRect(27, 100, 90, 21));
+	// Date/sunset setup
+	date_layer = text_layer_create(GRect(27, 98, 90, 21));
 	text_layer_set_text_color(date_layer, GColorWhite);
 	text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
 	text_layer_set_background_color(date_layer, GColorClear);
 	text_layer_set_font(date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
 	layer_add_child(window_layer, text_layer_get_layer(date_layer));
-
 	draw_date();
+
+	sunset_layer = text_layer_create(GRect(27, 118, 90, 17));
+	text_layer_set_text_color(sunset_layer, GColorWhite);
+	text_layer_set_text_alignment(sunset_layer, GTextAlignmentCenter);
+	text_layer_set_background_color(sunset_layer, GColorClear);
+	text_layer_set_font(sunset_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+	layer_add_child(window_layer, text_layer_get_layer(sunset_layer));
 
 	// Status setup
 	icon_battery = gbitmap_create_with_resource(RESOURCE_ID_BATTERY_ICON);
@@ -311,11 +271,6 @@ void init() {
 			&center_display_layer_update_callback);
 	layer_add_child(window_layer, center_display_layer);
 
-	second_display_layer = layer_create(GRECT_FULL_WINDOW);
-	layer_set_update_proc(second_display_layer,
-			&second_display_layer_update_callback);
-	layer_add_child(window_layer, second_display_layer);
-
 	// Configurable inverse
 #ifdef INVERSE
 	full_inverse_layer = inverter_layer_create(GRECT_FULL_WINDOW);
@@ -326,7 +281,6 @@ void init() {
 
 void deinit() {
 
-	window_destroy(window);
 	gbitmap_destroy(background_image_container);
 	gbitmap_destroy(icon_battery);
 	gbitmap_destroy(icon_battery_charge);
@@ -335,7 +289,6 @@ void deinit() {
 	layer_destroy(minute_display_layer);
 	layer_destroy(hour_display_layer);
 	layer_destroy(center_display_layer);
-	layer_destroy(second_display_layer);
 	layer_destroy(battery_layer);
 	layer_destroy(bt_layer);
 
@@ -344,10 +297,10 @@ void deinit() {
 #endif
 
 	layer_destroy(background_layer);
-	layer_destroy(window_layer);
-
 	gpath_destroy(hour_hand_path);
 	gpath_destroy(minute_hand_path);
+
+	layer_destroy(window_layer);
 }
 
 void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
@@ -369,26 +322,30 @@ void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
 				}
 			}
 		}
-
-		layer_mark_dirty(second_display_layer);
 	}
 }
 
-void conserve_power(bool conserve) {
-	if (conserve == g_conserve)
-		return;
-	g_conserve = conserve;
-	if (conserve) {
-		tick_timer_service_unsubscribe();
-		tick_timer_service_subscribe(MINUTE_UNIT, &handle_tick);
-		layer_set_hidden(second_display_layer, true);
+
+static void in_received_handler(DictionaryIterator *received, void * q) {
+	Tuple* tsucc = dict_find(received, CODE_SUNSET_SUCCESS);
+	Tuple* ttime = dict_find(received, CODE_SUNSET_TIME);
+
+	if (tsucc->value->int32) {
+		strncpy(sunset_text, ttime->value->cstring, sizeof(sunset_text));
 	} else {
-		tick_timer_service_unsubscribe();
-		tick_timer_service_subscribe(SECOND_UNIT, &handle_tick);
-		layer_set_hidden(second_display_layer, false);
+		strncpy(sunset_text, "ERR", sizeof(sunset_text));
 	}
+	draw_sunset();
 }
 
+static void init_mess() {
+	app_message_open(app_message_inbox_size_maximum(), 0);
+	app_message_register_inbox_received(&in_received_handler);
+}
+
+static void deinit_mess() {
+	app_message_deregister_callbacks();
+}
 
 
 /*
@@ -396,13 +353,13 @@ void conserve_power(bool conserve) {
  */
 int main(void) {
 	init();
-	tick_timer_service_subscribe(SECOND_UNIT, &handle_tick);
+	init_mess();
+	tick_timer_service_subscribe(MINUTE_UNIT, &handle_tick);
 	bluetooth_connection_service_subscribe(&bt_connection_handler);
-	battery_state_service_subscribe	(&battery_state_handler);
-
-	conserve_power(true);
+	battery_state_service_subscribe(&battery_state_handler);
 
 	app_event_loop();
+	deinit_mess();
 	deinit();
 }
 
---
+++ src/tx_generated.h
@@ -0,0 +1,6 @@
+#pragma once
+
+enum tx_codes {
+	CODE_SUNSET_SUCCESS = 1000100,
+	CODE_SUNSET_TIME = 1000101
+};
